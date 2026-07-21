6c7c2b140f0274daf98fffd793adbcb1384dcf85
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2026-07-22 00:12:21 +0530

    Create code.cpp

---
+++ Graph/BFS/code.cpp
@@ -0,0 +1,19 @@
+void BFS(int start, vector<vector<int>>& adj, vector<bool>& visited) {
+    queue<int> q;
+    
+    visited[start] = true;
+    q.push(start);
+    
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+        
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
